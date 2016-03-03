d3c4913349fbf8a09315f3f3c648bbfb92f668c2
Liv Kelley <Liv.Kelley@students.olin.edu>
2016-03-03 13:26:43 -0500

    Drawing rectangles.

--- main_game.py
+++ main_game.py
@@ -1,42 +1,72 @@
-import pygame, sys
+import pygame
 
-background = pygame.image.load(bif).convert()
-mouse_c=pygame.image.load(mif).convert_alpha()
 class model(object):
     """This is he model that stores the game state """
-
     class wall(object):
         """ This is the wall object (that pacman can't go through) in the game """
-
     class pacman(object):
         """ This is our pacman who dies from ghosts and eats balls!"""
-        def load_image(self, image_name)
-
-
     class ghost(object):
         """ this is a ghost that kills (and maybe chases) pacman"""
 
     class ball(object):
         """this is the ball that pacman eats and scores points by eating!"""
-        
+
+import pygame
+class model(object):
+    """This is he model that stores the game state """
+    class wall(object):
+        """ This is the wall object (that pacman can't go through) in the game """
+        def __init__(self, left, top, width, length):
+            self.left = left
+            self.top = top
+            self.width = width
+            self.length = length
+
+    class pacman(object):
+        """ This is our pacman who dies from ghosts and eats balls!"""
+        def __init__(self, x, y, lives):
+            self.x = x
+            self.y = y
+            self.lives = lives
+
+    class ghost(object):
+        """ this is a ghost that kills (and maybe chases) pacman"""
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+    class ball(object):
+        """this is the ball that pacman eats and scores points by eating!"""
+        def __init__ (self, x, y):
+            self.x = x
+            self.y = y
+
+    class point(object):
+        """ this is what you earn when you collect balls :)"""
+        def __init__ (self,number):
+            self.number = number
+        def add_point(self):
+            self.number = self.number + 1
+            return self.number # do I need to return this
+
 class controller (object):
     """ This is the controlled where user input (arrow keys or openCV) changes the model"""
 
 class pygameview (object):
     """ This is the game window drawing all of the things that the user seems in response to the changes in the model"""
 
-def drawGameFrame(boxx, boxy) 
-    lef, top = boxCoordinates(boxx,boxy)
-    pygame.draw.rect(DISPLAY, RED, (left-5, top = 5, (60,60), (60,60))) 
+pygame.init()
+display = pygame.display.set_mode([640, 600])
 
-pygame.init() 
-DISPLAY = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello Pygame!')
-    while True: 
-        for event in pygame.event.get(): 
-            if event.type == QUIT: 
-                pygame.quit() 
-                sys.exit 
- 
+red = (230, 50, 50) 
 
-    #     screen.blt(background, (0,0))
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+
+            
+window.fill((40,50,180)) 
+pygame.draw.rect(display, (red), Rect(100,300), (20,30)) 
+pygame.display.update() 
